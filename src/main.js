83ed041d977fc5621146b56fe062324d271207d8
CandyDream6 <tgmenggithub@163.com>
2025-07-06 01:47:26 +0800

    装个破组件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import './assets/main.css'  // 引入 main.css
 
@@ -15,5 +17,5 @@ const app = createApp(App)
 
 // 全局注册 Font Awesome 组件
 app.component('font-awesome-icon', FontAwesomeIcon)
-
+app.use(ElementPlus)
 app.mount('#app')
