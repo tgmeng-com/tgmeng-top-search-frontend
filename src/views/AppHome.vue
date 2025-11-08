7eadf34226d5857dbc936e74f64324fc9a80ca62
CandyDream6 <tgmenggithub@163.com>
2025-11-08 11:51:43 +0800

    fix:修改卡片为只有顶部可以拖动，卡片内容部分不可拖动

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -60,6 +60,7 @@
             item-key="title"
             class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
             :animation="300"
+            :handle="'.drag-handle'"
             @start="onDragStart"
             @end="onDragEnd"
         >
