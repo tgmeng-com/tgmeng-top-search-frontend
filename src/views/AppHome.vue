fdd2ec8b3b3fc90a3d60f484c6cd3b19ac6f80e4
CandyDream6 <tgmenggithub@163.com>
2025-11-11 02:47:33 +0800

    feat:增加定时器，前端定时自动刷新数据

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -49,7 +49,7 @@
         <!-- 右侧：更新时间（移动端换行显示） -->
         <div
             class="text-xs px-2 py-1 rounded-md bg-gray-300 dark:bg-gray-700 text-gray-600 dark:text-gray-300 whitespace-nowrap">
-          数据每分钟更新一次（GitHub数据每20-40分钟更新一次）
+          数据每分钟更新一次（GitHub20-40分钟，网易云音乐10-15分钟）
         </div>
       </div>
 
@@ -106,10 +106,22 @@ export default {
       umamiAllViews: this.$store.state.umamiAllViews,
       umamiAllTime: this.$store.state.umamiAllTime,
       preDragSortList: [], // 拖动前的 sort 列表
+      refreshTimer: null, // 定时器 ID
     };
   },
   async mounted() {
     this.initializePlatforms();
+    // 定时刷新数据
+    this.refreshTimer = setInterval(() => {
+      this.refreshData();
+    }, 30 * 1000); // 每30秒钟刷新一次，然后里面函数里判断数据是否是1分钟之前的
+  },
+  // 离开页面或者切换路由的时候，关闭定时器，避免造成内存泄露
+  beforeUnmount() {
+    if (this.refreshTimer) {
+      clearInterval(this.refreshTimer);
+      this.refreshTimer = null;
+    }
   },
   methods: {
     initializePlatforms() {
@@ -164,9 +176,9 @@ export default {
       if (cat.name === '收藏') {
         // 先清空收藏分类下的卡片
         this.activeCategory.subCategories.splice(0)
-        this.categroies[0].subCategories.forEach(cat => {
-          if (cat.isStar){
-            this.activeCategory.subCategories.push(cat)
+        this.categroies[0].subCategories.forEach(subCat => {
+          if (subCat.isStar) {
+            this.activeCategory.subCategories.push(subCat)
           }
         })
       }
@@ -179,6 +191,35 @@ export default {
           this.fetchData(subCat);
         }
       });
+    },
+
+    // 刷新当前分类下的数据
+    refreshData() {
+      this.activeCategory.subCategories.forEach(subCat => {
+        // 不同平台的时间不同，例如后台github数据是每20-40分钟刷新，那么前端就是判断github数据时间和当前时间相差40分钟的时候，再去主动更新
+        let singleUpdateTime = 60 * 1000;
+        switch (true) {
+          case subCat.title.includes('Star总榜') || subCat.title.includes('新仓库Star'):
+            singleUpdateTime = 40 * 60 * 1000; // 40分钟
+            break;
+          case subCat.title.includes('网易云'):
+            singleUpdateTime = 15 * 60 * 1000; // 15分钟
+            break;
+          default:
+            singleUpdateTime = 60 * 1000; // 默认1分钟
+        }
+        // 检查是否超过60秒没有更新
+        const updateTimestamp = new Date(subCat.updateTime.replace(/-/g, '/')).getTime();
+        if (subCat.isShow && new Date() - new Date(updateTimestamp) > singleUpdateTime) {
+          subCat.loading = true;
+          this.fetchData(subCat);
+        }
+        // 如果当前某个平台下数据为空，也主动刷新一下
+        if (subCat.isShow && subCat.data.length === 0) {
+          subCat.loading = true;
+          this.fetchData(subCat);
+        }
+      })
     },
 
     // 初始化全部分类，就是把其他分类下的东西放到全部分类下，方便展示
