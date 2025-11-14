53dcde65308e251ed8046af74b3d2816ab057546
CandyDream6 <tgmenggithub@163.com>
2025-11-14 15:41:02 +0800

    style:基础样式调整

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
     <main class="container mx-auto px-4 py-6">
       <!-- 这里放页面主体内容，比如 AppHome 组件 -->
       <Home/>
+      <!-- 主内容和评论区之间 -->
+      <GoogleAdsense ad-slot="9081541454" />
       <WalineComment/>
       <el-backtop :right="100" :bottom="100"/>
     </main>
@@ -17,6 +19,7 @@ import AppHeader from './components/Layout/AppHeader.vue';
 import AppHome from './views/AppHome.vue';
 import SettingSideBar from "@/components/SettingSideBar.vue"; // 假设 AppHome 是首页视图
 import WalineComment from "./components/WalineComment.vue";
+import GoogleAdsense from "./components/Adsense/Adsense.vue";
 
 export default {
   name: 'App',
@@ -25,6 +28,7 @@ export default {
     Header: AppHeader,
     Home: AppHome,
     WalineComment: WalineComment,
+    GoogleAdsense: GoogleAdsense,
   }
 }
 </script>
---
+++ src/components/Adsense/Adsense.vue
@@ -0,0 +1,38 @@
+<template>
+  <ins class="adsbygoogle"
+       style="display:block"
+       :data-ad-client="adClient"
+       :data-ad-slot="adSlot"
+       data-ad-format="auto"
+       data-full-width-responsive="true"></ins>
+</template>
+<script>
+export default {
+  name: "GoogleAdsense",
+  props: {
+    adClient: {
+      type: String,
+      default: "ca-pub-3286880109560525"
+    },
+    adSlot: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    // 确保脚本加载完成后再 push
+    this.$nextTick(() => {
+      if (window.adsbygoogle && window.adsbygoogle.push) {
+        try {
+          window.adsbygoogle.push({});
+        } catch (e) {
+          console.warn("Adsense error:", e);
+        }
+      }
+    });
+  }
+};
+</script>
+<style scoped>
+
+</style>
