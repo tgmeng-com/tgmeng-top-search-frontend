3c6cb205f0eaa0e7ee104dfe174dae0202d4b968
CandyDream6 <tgmenggithub@163.com>
2025-11-19 08:01:43 +0800

    style:停用多重ads

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
 <!--    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="9081541454" ad-format="auto"-->
 <!--                   :full-width-responsive="true"/>-->
     <WalineComment/>
-    <!-- 页面底部 多重Ads-->
-    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>
-    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>
-    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>
-    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>
+    <!-- 页面底部 多重Ads广告-->
+<!--    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>-->
+<!--    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>-->
+<!--    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>-->
+<!--    <GoogleAdsense ad-client="ca-pub-3286880109560525" ad-slot="7002721768" ad-format="autorelaxed"/>-->
     <el-backtop :right="100" :bottom="100"/>
   </div>
 </template>
